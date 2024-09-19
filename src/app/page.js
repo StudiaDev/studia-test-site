360cfefba9c077f1ae37e2d2f8b81eeb7d5b28ce
cesar-gamez <64483947+cesar-gamez@users.noreply.github.com>
2024-09-19 15:54:46 -0500

    completed left panel

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,30 @@ import ChapterInformationMediaContainer from "@/components/chapter-info/chapter-
 export default function Home() {
   return (
     <main className="dark">
-      <div>
+      <div className="flex-full">
         <Header />
-        <div className="hero-space">
-          <div className="left-panel">
-            <div className="left-panel-content">
-              <PipelineInformationContainer checkpoint="frontera-beta/v091824" model="gpt-4o" />
+        <div className="flex-panels">
+          <div className="left-flex-panel">
+            <div className="left-panel-element">
+              <PipelineInformationContainer
+                checkpoint="frontera-beta/v092324"
+                model="gpt-4o"
+              />
+              <RawChapterInformationContainer 
+                book="Introduction to Advanced Algorithms: Third Edition"
+                chapterNum={2}
+                pages={13}
+                words={8000}
+                time={0.75}
+              />
+            </div>
+            <div className="left-panel-element">
+              <ChapterInformationMediaContainer 
+                course={"CS 4349: Advanced Algorithm Design and Analysis"}
+                chapterNum={2}
+                pages={13}
+                image={"https://m.media-amazon.com/images/I/61O5SsbL8HL._AC_UF1000,1000_QL80_.jpg"}
+              />
             </div>
           </div>
         </div>
