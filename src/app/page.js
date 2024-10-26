1a86f725c2a3d584606363bf845cf8cfc074a0f1
Your Name <kaustuvkp19@gmail.com>
2024-10-26 14:07:01 -0500

    buttons added

--- src/app/page.js
+++ src/app/page.js
@@ -11,26 +11,34 @@ export default function Home() {
         <Header />
         <div className="flex-panels">
           <div className="left-flex-panel">
+            <h1>Welcome back, Tester</h1>
             <div className="left-panel-element">
               <PipelineInformationContainer
                 checkpoint="frontera-beta/v092324"
                 model="gpt-4o"
               />
               <RawChapterInformationContainer 
-                book="Introduction to Advanced Algorithms: Third Edition"
-                chapterNum={2}
-                pages={13}
-                words={8000}
-                time={0.75}
+                book="Global Conflicts Throughout History - 3rd Edition"
+                chapterNum={7}
+                pages={27}
+                words={13324}
+                time={2}
               />
             </div>
             <div className="left-panel-element">
               <ChapterInformationMediaContainer 
-                course={"CS 4349: Advanced Algorithm Design and Analysis"}
-                chapterNum={2}
-                pages={13}
-                image={"https://m.media-amazon.com/images/I/61O5SsbL8HL._AC_UF1000,1000_QL80_.jpg"}
+                course="Introduction to Machine Learning"
+                chapterNum={5}
+                pages={27}
+                image="https://m.media-amazon.com/images/I/61O5SsbL8HL._AC_UF1000,1000_QL80_.jpg"
               />
+            </div>
+          </div>
+          <div className="right-flex-panel">
+            <div className="page-change-buttons">
+              <button className="page-button left-button"> <svg width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.5L9.75 0.870834V12.1292L0 6.5Z" fill="white"/></svg></button>
+              <button className="page-button right-button"> <svg className = "triangle-right" width="10" height="13" viewBox="0 0 10 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 6.5L9.75 0.870834V12.1292L0 6.5Z" fill="white"/></svg>
+              </button>
             </div>
           </div>
         </div>
